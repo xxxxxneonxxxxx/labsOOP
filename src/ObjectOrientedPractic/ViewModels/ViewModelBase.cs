5a9b05296939523128906e84380269695c252405
xxxxxneonxxxxx <xneoshax@gmail.com>
2025-09-19 20:20:14 +0700

    начало делания лаб

---
+++ src/ObjectOrientedPractic/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ObjectOrientedPractics.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ src/ObjectOrientedPractic/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ObjectOrientedPractics.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
